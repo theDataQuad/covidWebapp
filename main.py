f953d476b9bea704a209d1669c9f2e5e2d104437
VaishU2235 <vaishaknarayanan619@gmail.com>
2021-07-30 01:14:10 +0530

    everything is now under if condition

--- main.py
+++ main.py
@@ -13,6 +13,19 @@ app = Flask(__name__)
 
 @app.before_first_request
 def startup():
+    def twoStates(slist,feature):
+            df=swave[swave.State.isin(slist)]
+            x = np.random.randint(low=0, high=5, size=150)
+
+            fig, ax = plt.subplots(figsize=(12, 7))
+            sns.lineplot(data=df, x="Date", y=feature, hue="State",linewidth = 2)
+            plt.legend( bbox_to_anchor=(1.02, 1),loc='upper left', borderaxespad=0, title='states')
+            plt.title(feature+' Comparrison')
+            plt.xticks(np.arange(0, len(x)+15, 20))
+            #ax.set_xticks(np.arange(0, len(x)+1, 5))
+            img = io.BytesIO()
+            plt.savefig(img, format='png')
+            return(img.seek(0))
     df=pd.read_csv('states18.csv')
     statelist=df['State'].unique()
     c=df.columns
@@ -66,27 +79,17 @@ def startup():
 #plt.xticks(np.arange(0, len(x)+15, 20))
 #ax.set_xticks(np.arange(0, len(x)+1, 5))
 #plt.show
+DEFAULT_STATE1='India'
+DEFAULT_STATE2='Kerala'
+
 @app.route("/", methods=['POST', 'GET'])
 def submit_new_request():
-    selected_state1 = request.form['selected_state1']
-    selected_state2 = request.form['selected_state2']
-    states=[selected_state1,selected_state2]
-    feature='Confirmed'
-    def twoStates(slist,feature):
-            df=swave[swave.State.isin(slist)]
-            x = np.random.randint(low=0, high=5, size=150)
-
-            fig, ax = plt.subplots(figsize=(12, 7))
-            sns.lineplot(data=df, x="Date", y=feature, hue="State",linewidth = 2)
-            plt.legend( bbox_to_anchor=(1.02, 1),loc='upper left', borderaxespad=0, title='states')
-            plt.title(feature+' Comparrison')
-            plt.xticks(np.arange(0, len(x)+15, 20))
-            #ax.set_xticks(np.arange(0, len(x)+1, 5))
-            img = io.BytesIO()
-            plt.savefig(img, format='png')
-            return(img.seek(0))
-    img=twoStates(states,feature)
     if request.method == 'POST':
+        selected_state1 = request.form['selected_state1']
+        selected_state2 = request.form['selected_state2']
+        states=[selected_state1,selected_state2]
+        feature='Confirmed'
+        img=twoStates(states,feature)
         plot_url = base64.b64encode(img.getvalue()).decode()
         return render_template('index.html',
                 model_plot = Markup('<img src="data:image/png;base64,{}">'.format(plot_url)),
@@ -95,8 +98,8 @@ def submit_new_request():
     else:
         return render_template('index.html',
             model_plot = '',
-            selected_state1=selected_state1,
-            selected_state2=selected_state2)
+            selected_state1=DEFAULT_STATE1,
+            selected_state2=DEFAULT_STATE2)
 
 if __name__=='__main__':
 	app.run(debug=False)
