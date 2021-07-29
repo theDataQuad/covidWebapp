46b44bca56c74905fe2bff5d39f7a4fbaeb5765b
VaishU2235 <vaishaknarayanan619@gmail.com>
2021-07-30 01:49:02 +0530

    twoStates is now defined in two places, inside if and ouside everything

--- main.py
+++ main.py
@@ -11,11 +11,7 @@ import io, base64, os
 
 matplotlib.use('Agg')
 app = Flask(__name__)
-
-
-@app.before_first_request
-def startup():
-    def twoStates(slist,feature):
+def twoStates(slist,feature):
             df=swave[swave.State.isin(slist)]
             x = np.random.randint(low=0, high=5, size=150)
 
@@ -28,6 +24,9 @@ def startup():
             img = io.BytesIO()
             plt.savefig(img, format='png')
             return(img.seek(0))
+
+@app.before_first_request
+def startup():
     df=pd.read_csv('states18.csv')
     statelist=df['State'].unique()
     c=df.columns
@@ -87,6 +86,19 @@ DEFAULT_STATE2='Kerala'
 @app.route("/", methods=['POST', 'GET'])
 def submit_new_request():
     if request.method == 'POST':
+        def twoStates(slist,feature):
+            df=swave[swave.State.isin(slist)]
+            x = np.random.randint(low=0, high=5, size=150)
+
+            fig, ax = plt.subplots(figsize=(12, 7))
+            sns.lineplot(data=df, x="Date", y=feature, hue="State",linewidth = 2)
+            plt.legend( bbox_to_anchor=(1.02, 1),loc='upper left', borderaxespad=0, title='states')
+            plt.title(feature+' Comparrison')
+            plt.xticks(np.arange(0, len(x)+15, 20))
+            #ax.set_xticks(np.arange(0, len(x)+1, 5))
+            img = io.BytesIO()
+            plt.savefig(img, format='png')
+            return(img.seek(0))
         selected_state1 = request.form['selected_state1']
         selected_state2 = request.form['selected_state2']
         states=[selected_state1,selected_state2]
