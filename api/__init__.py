c5dd6df46b9a1b62d3bd7b4810e0844ca991270a
VaishU2235 <vaishaknarayanan619@gmail.com>
2021-08-13 18:15:22 +0530

    Death Rate added

--- api/__init__.py
+++ api/__init__.py
@@ -4,7 +4,8 @@ url_vac='http://api.covid19india.org/csv/latest/vaccine_doses_statewise_v2.csv'
 url_day='https://api.covid19india.org/csv/latest/states.csv'
 df=pd.read_csv(url_day)
 print("Contact with API")
-#global statelist
+
+#TODO: clarify about state unassigned
 statelist=df['State'].unique()
 ndf = pd.DataFrame(columns=df.columns)
 for state in statelist:
@@ -20,31 +21,23 @@ ndf.to_csv('daily.csv',index=False)
 print('daily covid data saved!!')
 
 vac=pd.read_csv(url_vac)
-#change total to india
+
 vac.to_csv('vac_daily.csv',index=False)
 print('daily vaccination data saved!!')
 
 sumvac=vac.tail(38)
+sumvac.reset_index(inplace=True)
+sumvac['State'][37]="India"
 
 pop=pd.read_csv('projected_population2021.csv')
-
-##remove
-pop.rename({'State or union territory':'State'},axis=1,inplace=True)
-pop=pop.drop('Rank',axis=1)
-pop['State'][35]="Jammu and Kashmir"
-pop['State'][31]="Andaman and Nicobar Islands"
-pop['State'][32]="Dadra and Nagar Haveli and Daman and Diu"
-pop['State'][37]="Total"
-pop.rename({'Population (2021 estimates)':'Population'},axis=1,inplace=True)
-pop['Population'][32]="585764"
-pop['Population']=pop['Population'].str.replace(',','').astype(int)
 
 df=pd.merge(pop,sumvac,on='State',how='inner')
 df=df.drop('Vaccinated As of',axis=1)
 df['Not_Vaccinated']=df['Population']-df['First Dose Administered']
 df.rename({'First Dose Administered':'Partially_Vaccinated','Second Dose Administered':'Fully_Vaccinated'},axis=1,inplace=True)
 df.drop('Total Doses Administered',axis=1,inplace=True)
-df['Partially_Vaccinated1']=df['Partially_Vaccinated']-df['Fully_Vaccinated']
+
+df['Partially_Vaccinated']=df['Partially_Vaccinated']-df['Fully_Vaccinated']
 
 df.to_csv('pop_with_vac.csv',index=False)
 print('population data saved!!')
