a4ebfbb6e509db4f88acd8545f310a4a11bfee28
VaishU2235 <vaishaknarayanan619@gmail.com>
2021-08-30 20:48:39 +0530

    Front page dark theme added

--- viz/plt.py
+++ viz/plt.py
@@ -26,6 +26,9 @@ def pie_chart(State):
     data_to_plot = [trace]
     layout_one = go.Layout(height=900, width=900,title=go.layout.Title(text='Vaccination status of '+State))
     pie_chart = go.Figure(data = data_to_plot,layout=layout_one)
+    pie_chart.update_layout(paper_bgcolor="#113340")
+    pie_chart.layout.legend.font.color = 'white'
+    pie_chart.layout.titlefont.color = 'white'
     return pie_chart
 
 def barChart():
@@ -33,6 +36,12 @@ def barChart():
     last37=last37.tail(37)
     last37.sort_values(by='Death_Rate',inplace=True,ascending=False)
     data_to_plot = [go.Bar(x =last37['State'],y =last37['Death_Rate'] ,orientation="v")]
-    layout=go.Layout(height=900, width=900,title=go.layout.Title(text="Statewise Death Rates"))
+    layout=go.Layout(height=900, width=900,title=go.layout.Title(text="Statewise Death Rates"),plot_bgcolor='#113340')
     fig = go.Figure(data=data_to_plot,layout=layout)
+    fig.update_layout(paper_bgcolor="#113340")
+    fig.layout.xaxis.color = 'white'
+    fig.layout.yaxis.color = 'white'
+    fig.layout.titlefont.color = 'white'
+    fig.update_xaxes(showgrid=False)
+    fig.update_yaxes(showgrid=False)
     return fig
